2a99dd54d33be60f1fcad3e562c42e11021a58c5
chaasfr <chaasfr@gmail.com>
2015-10-04 22:05:27 +0200

    fixed some issues

--- hmm.py
+++ hmm.py
@@ -1,6 +1,7 @@
 # coding=utf-8
 import nltk.probability
 import numpy
+from numpy import *
 
 class HMM(object):
 
@@ -17,16 +18,16 @@ class HMM(object):
     # Entrées:
     # Seq: la sequence de mots étudiée
     #**********************
-    def Viterbi(self,seq): #inspiré de hmm_mit_simple
+    def viterbi(self,seq): #inspiré de hmm_mit_simple
         T=len(seq)
         N=len(self._states)
-        Score = zeros((T, N), float64) #Matrice de calcul des scores temporaires. V[t,Sj]= score d'arriver en Sj au temps t
-        B = zeros(T,N) #Matrice des paths temp. B[t,Sj]=Si <=> si on est à l'état Sj au temps t, l'état le plus probable suivant est Si
+        V = zeros((T, N), float64) #Matrice de calcul des scores temporaires. V[t,Sj]= score d'arriver en Sj au temps t
+        B = zeros((T,N),float64) #Matrice des paths temp. B[t,Sj]=Si <=> si on est à l'état Sj au temps t, l'état le plus probable suivant est Si
 
         # Les probabilités de débuter de chaque état
         symbol = seq[0]
         for i, state in enumerate(self._states):
-            V[0, i] = self._pi(state) + self._E(state, symbol)
+            V[0, i] = self._pi[state] + self._E[state, symbol]
             B[0, state] = None
 
         # Cherche l'état avec la plus grande proba au temps t
@@ -40,7 +41,7 @@ class HMM(object):
                     va = V[t-1, i] + self._T[j,i]
                     if not best or va > best[0]:
                         best = (va, si)
-                V[t, j] = best[0] + self._E(sj, symbol)
+                V[t, j] = best[0] + self._E[sj, symbol]
                 B[t, sj] = best[1]
 
         # Cherche l'état final
@@ -76,12 +77,12 @@ class HMMTrainer(object):
 # E la liste des scores d'émission
 # compte le nombre d'apparition en premier mot(pi), le nombre de bigramme(transition) et d'observation (emission)
 #**********************
-    def ModeleGeneratif(self,sequenceX,sequenceY):
-        # update de la liste des états et des symboles du HMM
+    def modeleGeneratif(self,sequenceX,sequenceY):
+        # update de la liste des états et des symboles du HMM. A optimiser
         for i in range(len(sequenceX)):
             xs=sequenceX[i]
             ys=sequenceY[i]
-            for j in range(xs):
+            for j in range(len(xs)):
                 state= ys[j]
                 symbol= xs[j]
                 if state not in self._states:
@@ -90,8 +91,8 @@ class HMMTrainer(object):
                     self._symbols.append(symbol)
         #Compte les occurences, les transitions, les emissions et les starters de chaque phrase
         first = [0]*len(self._states)
-        T = zeros(len(self._states),len(self._states))
-        E = zeros(len(self._states),len(self._symbols))
+        T = zeros((len(self._states),len(self._states)),float64)
+        E = zeros((len(self._states),len(self._symbols)),float64)
         occurence=[0]*len(self._states)
         for i in range(len(sequenceX)):
             lasts = -1
@@ -102,13 +103,13 @@ class HMMTrainer(object):
                 symbol = xs[j]
                 occurence[state] +=1; #compte le nombre d'occurence de state
                 if lasts == -1: #si le mot débute la phrase
-                    starting[state] +=1 #compte le nombre de fois que state commence une phrase
+                    first[state] +=1 #compte le nombre de fois que state commence une phrase
                 else:
                     T[lasts][state] +=1 #compte le nombre de fois qu'on passe de lasts à state
                 E[state][symbol] +=1 # compte le nombre de fois qu'on étiquette symbol par state
                 lasts = state
         #calcul les probabilités
-        pi = first/len(sequenceX) #retourne pi cf énoncé
+        pi = [x / len(sequenceX) for x in first] #retourne pi cf énoncé
         for i in range(len(self._states)):
             for j in range(len(self._states)):
                 T[i,j] = T[i,j]/occurence[i]
--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 # coding=utf-8
-import hmm
+import numpy
+from hmm import HMMTrainer
+from hmm import HMM
 #**********************
 # Lecture des donnees
 # Retourne deux listes X et Y avec:
@@ -30,3 +32,16 @@ def lireData(sourceFile):
 
 Xtrain,Ytrain=lireData("Data/ftb.train.encode")
 Xdev,Ydev=lireData("Data/ftb.dev.encode")
+nb_symboles= numpy.max([numpy.max(U) for U in Xtrain])
+nb_states = numpy.max([numpy.max(U) for U in Ytrain])
+
+#Xtrain=[[1, 2, 1, 1, 3, 5, 6, 7, 1, 1, 2], [2, 1, 0, 3, 1, 4, 7, 6, 2, 0, 1] , [0, 1, 1, 2, 5, 7, 7, 7, 1, 1, 1]]
+#Ytrain=[[0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0], [0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0],  [0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0]]
+#nb_states = 2
+#nb_symboles = 8
+
+trainer = HMMTrainer(range(nb_states), range(nb_symboles))
+m= trainer.modeleGeneratif(Xtrain, Ytrain)
+
+l = m.viterbi(Xtrain[600])
+print('Xtrain[600]=',Xtrain[600],' l=', l)
